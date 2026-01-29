bbc7a956c5d4378f9d5b9f177c7996a91a47d0b3
Farhan Jiwani <farhan@farhanjiwani.com>
2026-01-29 13:43:44 -0500

    (chore) initial setup

---
+++ nuxt.config.ts
@@ -0,0 +1,25 @@
+import { defineNuxtConfig } from "nuxt/config";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  css: ["~/assets/css/main.scss"],
+  devtools: { enabled: true },
+
+  modules: [
+    "@nuxt/a11y",
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/hints",
+    "@nuxt/image",
+    "@nuxt/scripts",
+    "@nuxtjs/tailwindcss",
+    "@nuxt/test-utils",
+    "@pinia/nuxt",
+  ],
+
+  typescript: {
+    typeCheck: false, // check at CI/CD & troubleshooting dev via npm script
+    strict: true,
+  },
+});
